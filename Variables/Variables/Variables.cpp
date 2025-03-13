d8473d4aee3744c298b662deee1958ba8411a820
EngineerGamer <115916718+ThisIsEngineerGaming@users.noreply.github.com>
2025-03-13 16:20:33 +0200

    Add files via upload

---
+++ Variables/Variables/Variables.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+using namespace std;
+// Exercise 1
+//int main()
+//{
+//	cout << "Enter 2 numbesr becaue yeg\n";
+//	int number1;
+//	int number2;
+//	cin >> number1;
+//	cin >> number2;
+//	cout << "This is what you get if you add those together: " << number1 + number2 << "\n";
+//	cout << "This is what you get if you subtract those two numbers: " << number1 - number2 << "\n";
+//	cout << "This is what you get if you multiply those two numbers: " << number1 * number2 << "\n";
+//}
+// Exercise 2
+//int main()
+//{
+//	cout << "Enter 2 numbesr becaue yeg\n";
+//	int number;
+//	int percent;
+//	cin >> number;
+//	cin >> percent;
+//	cout << "This is what you get: " << (number * percent) / 100 << "\n";
+//}
+// Exercise 3
+//int main()
+//{
+//	cout << "Enter 2 numbesr becaue yeg\n";
+//	int a;
+//	int b;
+//	cin >> a;
+//	cin >> b;
+//	cout << "This is what you get: " << a * b << "\n";
+//}
+// Exercise 4
+//int main()
+//{
+//	cout << "Enter the temp in celsius\n";
+//	int celsius;
+//	cin >> celsius;
+//	cout << "This is the temp in fahrenheit: " << celsius * 9 / 5 + 32 << "\n";
+//}
+// Exercise 5
+//int main()
+//{
+//	cout << "Enter 3 numbers\n";
+//	int number1;
+//	cin >> number1;
+//	int number2;
+//	cin >> number2;
+//	int number3;
+//	cin >> number3;
+//	cout << "The average is: " << (number1 + number2 + number3) / 3 << "\n";
+//}
+// Exercise 6
+int main()
+{
+	cout << "Enter the radius\n";
+	const double PI = 3.1415926;
+	int radius;
+	cin >> radius;
+	cout << "The length is: " << 2 * PI * radius << "\n";
+}
